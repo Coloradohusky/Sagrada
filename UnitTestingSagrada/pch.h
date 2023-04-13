c64bd9ada176a4c4ae1cd2a6679307aa3c89a166
justen <45608802+javds@users.noreply.github.com>
2023-04-13 13:19:17 -0700

    fixed and commented the google test
    gave some pointers on how to use the google test framework that is in the project as well as set the unit test project  to include sagradas files

--- UnitTestingSagrada/pch.h
+++ UnitTestingSagrada/pch.h
@@ -5,3 +5,7 @@
 #pragma once
 
 #include "gtest/gtest.h"
+#include "Player.hpp"
+
+
+//note on how to use this... include the h files of the stuff you want to test and then design the tests in test.cpp of this project
--- UnitTestingSagrada/test.cpp
+++ UnitTestingSagrada/test.cpp
@@ -1,6 +1,16 @@
 #include "pch.h"
 
+//note if you cant use classes like usual make sure to include them in the PCH file or precompiled header file
+
 TEST(TestCaseName, TestName) {
+
+  //Expect_EQ stands for expect two things are equal.
+  //expect is non fatal meaning if this is wrong the test continues through but its still a fail 
   EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+
+  //assert eq stands for assert two things are equal
+  //failing this is fatal test stops immediately and fails
+  ASSERT_EQ(true, true);
 }
+
+
