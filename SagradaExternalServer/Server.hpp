12213211f69ee5ba288bd8276d929fc7175f7061
justen <45608802+javds@users.noreply.github.com>
2023-04-26 12:49:55 -0700

    commented network code

--- SagradaExternalServer/Server.hpp
+++ SagradaExternalServer/Server.hpp
@@ -1,3 +1,12 @@
+////////////////////////////////////////////////////////////////////////////////////////////
+//name: Justin Van Der Sluys															  //
+//last edited: 4/26/2023																  //
+//file: Server.hpp																		  //
+//description: Server class that handles the data from the sagrada						  //
+//game. its essentially a csv storage server as it can only get LeaderBoardEntrys		  //
+//and store them into the csv file.														  //
+////////////////////////////////////////////////////////////////////////////////////////////
+//help from this video series https://www.youtube.com/playlist?list=PLk6CEY9XxSIAeK-EAh3hB4fgNvYkYmghp
 #pragma once
 #include "Bundler.hpp"
 #include "Includer.hpp"
@@ -5,11 +14,12 @@
 
 struct sendData 
 {
+	//setting the sendFlag tells the send thread its ready to go and it will send the message immediately this doesnt matter much since I was told to omit sending on the server
 	bool sendFlag;
 	char sendMsg[1024];
 };
 
-
+//expected packets
 //(pst,p1,p1score,true/false,p2,p2score,true/false,p3,p3score,true/false,p4,p4score,true/false,)
 struct LeaderBoardEntry 
 {
@@ -36,33 +46,78 @@ struct LeaderBoardEntry
 
 
 
-
-
 class Server
 {
 public:
 	Server(int listenPort);
 	~Server();
 
-	std::stringstream* getOutStream();
-
+	//description: waits for the server to connect then starts the send and receive threads ends the threads when the server disconnects and re sets up and waits for more connections
+	//special considerations: Server needs to be end to end routable with a open port of 46580 to work I used Hamachi to establish a pseudo lan if things are really not working 
+	//use wireshark and packet sender to isolate a problem
+	//params:none
+	//return:void
+	//pre-conditions:none
+	//post conditions:none
 	void start();
 
 private:
+
+	//description:runs in its own thread basically waits to receive data from the socket when and if it does it reads it into a incomingdata buffer and then calls
+	//read messages to handle the data
+	//params:none
+	//return:none
+	//pre-conditions:the socket should be ok and connected for this to work
+	//post conditions:none
 	void receive();
 
+	//description:runs in its own thread checks to see if the user has filled the sendmessage buffer by looking at the true or false sendflag
+	//params:none
+	//return:none
+	//pre-conditions: if the buffer that is filled does not have a \0 somewhere in bounds it will crash it uses strlen
+	//post conditions:none
 	void send();
 
+	//description:general function that reads a char array of a particular size into a stream it then looks for a delimiter and if it is encountered it increments the waiting packets ticker
+	//params:char arr[], int sizearr, std::stringstream& stream,char packetDelim, int& waitingPackets
+	//return: none
+	//pre-conditions:sizearr should be the valid size and the stream must not be in a error state
+	//post conditions:none
 	void readbufferIntoStream(char arr[], int sizearr, std::stringstream& stream,char packetDelim, int& waitingPackets);
 
+	//description:writes the scoreboard data to a csv file
+	//params:none
+	//return:none
+	//pre-conditions:none
+	//post conditions:none
 	void writeToCsv();
 
+	//description:reads the scoreboard csv into the leaderboard array
+	//params:none
+	//return:none
+	//pre-conditions:none
+	//post conditions:csv data should then added to the leaderboard
 	void readCsv();
 
+	//description:DO NOT USE not thread safe
+	//params:none
+	//return:none
+	//pre-conditions:none
+	//post conditions:should read segments of the stringstream into the sendbuf and set it to send data
 	void readIntoOutstream();
 
+	//description: DO NOT USE server does not send messages only receives
+	//params: none
+	//return: none
+	//pre-conditions: none
+	//post conditions: sendarray should be populated and set to send
 	void prepSendBuf();
 
+	//description:reads the incoming instream untill there are no more fully formed packets inside of it.
+	//params:none
+	//return:none
+	//pre-conditions:should have packets to read
+	//post conditions:if any packets came in it should have added more scoreboard entries
 	void readMessages();
 
 	bool keepRunning;
@@ -70,14 +125,20 @@ private:
 
 	std::vector<LeaderBoardEntry> LeaderBoard;
 
+	//send data
 	sendData sendBuf;
 
 	//socket information
 	int listenPort;
 	sf::TcpSocket serverSock;
 
+	//how many complete packets are waiting in inStream
 	int waitingPackets;
+
+	//this only gets touched by one thread at a time so its fine to use
 	std::stringstream inStream;
+
+	//realized this should be using a mutex and locking access but hindsight is 20/20 for the sake of not bricking the project I will stick to char arrays
 	std::stringstream outStream;
 };
 
--- SagradaTcpClient/SagradaTcpClient.hpp
+++ SagradaTcpClient/SagradaTcpClient.hpp
@@ -1,3 +1,13 @@
+//////////////////////////////////////////////////////////////////////////////////
+//name: Justin Van Der Sluys													//
+//date last edited: 4/26/2023													//
+//file: SagradaTcpClient.hpp													//
+//description:Sagrada Tcp client basically handles sending of data to a server 	//
+//that keeps track of the match history											//
+//																				//
+//																				//
+//////////////////////////////////////////////////////////////////////////////////
+
 #pragma once
 //bundler is my personal set of methods
 #include "Bundler.hpp"
@@ -13,34 +23,74 @@ struct WinLossEntry
 
 };
 
+
+//networking is slowly driving me insane
 class SagradaTcpClient
 {
 public:
 	SagradaTcpClient(std::string remoteAddr_, int remotePort_);
 
-
+	//description:tries to establish a connection with the server when it does it starts the send and receive threads
+	//params:none
+	//return:none
+	//pre-conditions:none
+	//post conditions:none
 	void startServer();
+
+	//description:kills the server disconnects the server basically used to kill the threads and rejoin
+	//params:none
+	//return:none
+	//pre-conditions:none
+	//post conditions:none
 	void killserver();
+
+	//description: pushes a string to the send buffer and sets the sendflag to go
+	//params: const char* string
+	//return:nothing
+	//pre-conditions: string should have a /0 in it
+	//post conditions: string should be sent by the buffer
 	void sendMessage(const char* string);
 
 private:
 
+	//description:threaded function that continuously checks to see if the send flag is set and then sends the buffer
+	//params:none
+	//return:none
+	//pre-conditions: socket should be ready to go and connected
+	//post conditions: none
 	void send();
 
+	//description: threaded fucntion that continously tries to receive a message from the server then it reads it and deals with it
+	//params:void
+	//return:void
+	//pre-conditions:socket should be ready to go and connected
+	//post conditions:void
 	void receive();
 
+	//description:reads a array into a stream and then increments the waiting packets according to how many delimiters were found
+	//params:char arr[], int sizearr, std::stringstream& stream, char packetDelim, int& waitingPackets
+	//return:void
+	//pre-conditions:none
+	//post conditions:none
 	void readbufferIntoStream(char arr[], int sizearr, std::stringstream& stream, char packetDelim, int& waitingPackets);
 
+	//description: reads the incoming stream buffer and handles the data
+	//params:none
+	//return:none
+	//pre-conditions:none
+	//post conditions:none
 	void readIncomingBuffer();
 
 	bool connected;
 	bool keepRunning;
+
+
 	int inboundMessagesWaiting;
 
 	std::stringstream incomingStream;
 	std::stringstream outBoundStream;
 
-	//there must be a \n in this or it will be a problem
+	//there must be a \0 in this or it will be a problem
 	bool sendFlag;
 	char sendBuf[1024];
 	
